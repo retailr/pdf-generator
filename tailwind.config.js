df4f7a961170515f97c42041726f74329272aa3c
Nimisoere Lawson <lawson.nimi@yahoo.com>
2020-12-11 13:12:18 +0100

    Add deploy config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.ts', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
